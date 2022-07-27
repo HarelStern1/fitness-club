2fa1e09485eb556a22eb1c224668e6f68945c2cb
Harel Stern <harel@Harels-MacBook-Pro.local>
2022-07-27 04:27:28 +0300

    changed font size

--- pages/_document.js
+++ pages/_document.js
@@ -37,6 +37,12 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap"
             rel="stylesheet"
           />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@100;300&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
